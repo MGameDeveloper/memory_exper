f1fa50a65afcbc5e939ee1d24c84c572e3125888
MGameDev <riggingmaker@gmail.com>
2023-01-11 22:37:16 +0300

    mapped win32 virtual-keys

---
+++ common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "utils/Memory.h"
+
+#include "utils/Containers/stack.h"
+#include "utils/Containers/Queue.h"
+#include "utils/Containers/Array.h"
+
+#include "keys.h"
