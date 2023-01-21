2a232cf37ac263273b91c1bcee1eb222b6544542
MGameDev <riggingmaker@gmail.com>
2023-01-21 22:33:56 +0300

    organized folders

--- platform.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern bool platform_init();
-extern bool platform_deinit();
-extern void platform_run();
-extern bool platform_closing();
--- win32/xinput_interface.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-extern void  xinput_init();
-extern void  xinput_process();
-extern void  xinput_devices_changed();
